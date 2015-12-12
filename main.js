2152ebeca83a61622c61b8c4776d270b77564b84
Sendrix2000 <montcalmt@gmail.com>
2015-12-12 12:32:32 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 time = 60; //the time the player have before game over
 var timeChange = 1; //the time being lost every second
-setInterval(function() {
-  time -= timeChange;
-},1000); //substractin timeChange from time every second
-setInterval(function() {
+window.setInterval(function(){
+  time -= timeChange
+}, 1000); //substractin timeChange from time every second
+window.setInterval(function(){
   document.getElementById("time").innerHTML = time;
-},1000); //updating time every second
+}, 1000); //updating time every second
