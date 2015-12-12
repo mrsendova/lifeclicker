75095a13f257017cf7dca92771b312066fce81e7
Sendrix2000 <montcalmt@gmail.com>
2015-12-12 12:15:59 -0500

    Update main.js
    The player will now see his new time every second.
    Minor bug fix

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-time = 60;
-var value = 1;
+time = 60; //the time the player have before game over
+var timeChange = 1; //the time being lost every second
+setInterval(function() {
+  time -= timeChange;
+},1000); //substractin timeChange from time every second
 setInterval(function() {
   document.getElementById("time").innerHTML = time;
-  time -= value;
-},1000);
-
+},1000); //updating time every second
