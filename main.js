e2437d3edaf2a4f70f19a4f6c7773d79d1f95818
Raphaël M-P <rmarquispelletier@gmail.com>
2015-12-12 15:28:21 -0500

    Added License to JS file

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 /* Copyright 2015 Raphaël M-P & Timothée Montcalm
+
+This file is part of LifeClicker.
+
+LifeClicker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with LifeClicker.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 time = 60; //the time the player have before game over
 var timeChange = 1; //the time being lost every second
-var time
 
 function changeTime(newtime){
   document.getElementById("time").innerHTML = newtime;
