7ddbe8c74c391046e3b2c25d7b03845dc4d91e31
Sendrix2000 <montcalmt@gmail.com>
2015-12-27 19:12:32 -0500

    Made a second button that makes the first one better, add ressource change indicator on both buttons add an unfriendly message when your time gets under 0 and reset time to 120 seconds.

--- main.js
+++ main.js
@@ -18,17 +18,41 @@ along with LifeClicker.  If not, see <http://www.gnu.org/licenses/>.
 
 var time = 60; //the time the player have before game over
 var timeChange = 1; //the time being lost every second
+var clickPower = 1;
 
-function refreshData(a){
-  document.getElementById("a").innerHTML = a;
+function checkTime(){
+  if (time <= 0) {
+    time = 120;
+    alert("you suck... here's 2 minutes for your troubles.");
+  }
+}
+
+function refreshTime(newTime){
+  document.getElementById("time").innerHTML = newTime;
 }
 
 setInterval(function() {
   time -= timeChange;
-  refreshData(time);
+  refreshTime(time);
+  checkTime();
 },1000); //substractin timeChange from time every second
 
 function buttonClicked(){
-  time += 1;
-  refreshData(time);
+  time += clickPower;
+  refreshTime(time);
+}
+
+//from now on upgradeClickPower will be shorten in ucp except for the name of the function
+//from now on incrementation will be shorten in inc which will not stand for incorporated like the usual contraction does
+ucpPrice = 10; //the price of the upgrade
+ucpPriceInc = 10 //the number of % the price goes up every time the upgrade is bought
+
+function upgradeClickPower(){
+  time -= ucpPrice;
+  refreshTime(time);
+  ucpPrice = Math.round(ucpPrice*(ucpPriceInc/100+1));
+  clickPower += 1;
+  document.getElementById("ucpPrice").innerHTML = ucpPrice;
+  document.getElementById("clickPower").innerHTML = clickPower;
+  checkTime();
 }
